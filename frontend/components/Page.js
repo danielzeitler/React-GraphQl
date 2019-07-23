526f6f04a7a9486e15b59ef385e020ef846404f3
Daniel <dz@danielzeitler.de>
2019-07-23 13:49:03 +0200

    feat: setting up next.js with boilerplate code

---
+++ frontend/components/Page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from "./Header.js"
+import Meta from "../components/Meta";
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        { this.props.children }
+      </div>
+    )
+  }
+}
+export default Page
